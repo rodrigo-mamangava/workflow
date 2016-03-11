63690933a13b0a820af5e04fc8d1d433978faa09
rodrigo-mamangava <rodrigo@mamangava.com>
2016-03-11 17:13:39 -0300

    coffee processado

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function (){
-	gutil.log('Workssss!');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+
+gulp.task('coffee', function (){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
+
 });
+
